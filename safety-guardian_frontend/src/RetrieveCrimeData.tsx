3689787f8623e0a551395eda2fc00d47944c3032
Hua <hua.void@gmail.com>
2022-10-29 16:15:22 -0400

    Add crime data heatmap display module
    1. Add heatmap display module
    2. Add crime data fetching module

--- safety-guardian_frontend/src/RetrieveCrimeData.tsx
+++ safety-guardian_frontend/src/RetrieveCrimeData.tsx
@@ -1,16 +1,46 @@
 import React from "react";
 import axios from "axios";
 
-const url = "http://localhost:8080/crime_data";
+const URL = "http://localhost:8080/crime_data";
+const SIZE = "?size=3000";
 
-export default function getCrimeData(): void {
-  let spots;
-  axios
-    .get(url)
+export default async function getCrimeData(): Promise<Array<[number, number, number]>> {
+  let spots: Array<[number, number, number]> = [];
+  let page: number = 1;
+  let totalPages = 0;
+
+  await axios
+    .get(URL + SIZE + "&page=0")
     .then((response) => {
-      console.log(response.data);
+      let res = response.data;
+      totalPages = res.page.totalPages;
+      spots = extractCoords(res._embedded.crimeDataList);
     })
     .catch((error) => {
       console.log("Retrieve crime data error: " + error);
     });
+
+  while (page < totalPages) {
+    await axios
+      .get(URL + SIZE + "&page=" + page.toString())
+      .then((response) => {
+        spots = spots.concat(extractCoords(response.data._embedded.crimeDataList));
+        console.log(spots.length);
+      })
+      .catch((error) => {
+        console.log("Retrieve crime data error: " + error);
+      });
+    page++;
+  }
+
+  return await spots;
+}
+
+function extractCoords(crimeDataList: Array<any>): Array<[number, number, number]> {
+  let coords: Array<[number, number, number]> = [];
+  for (let crimeData of crimeDataList) {
+    coords.push([crimeData.y, crimeData.x, crimeData.crimeType.weight]);
+  }
+
+  return coords;
 }
